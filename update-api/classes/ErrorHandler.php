cad654596aacb970e78f19a0c40aad5258d3935c
nikolai@vontainment.com <nikolai@vontainment.com>
2025-07-05 17:12:14 -0400

    modified:   update-api/classes/ErrorHandler.php

--- update-api/classes/ErrorHandler.php
+++ update-api/classes/ErrorHandler.php
@@ -96,7 +96,7 @@ class ErrorHandler // @phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamesp
      */
     public static function logMessage(string $message, string $type = 'error'): void
     {
-        $logFile = __DIR__ . '/../php_app.log';
+        $logFile = __DIR__ . '/../storage/logs/php_app.log';
         $timestamp = date("Y-m-d H:i:s");
         $logMessage = "[$timestamp] [$type]: $message\n";
         error_log($logMessage, 3, $logFile);
