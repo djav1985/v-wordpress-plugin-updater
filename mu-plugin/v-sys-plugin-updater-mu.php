e2f56546b133ea688b396d649fe6c4e2246e00a3
Vontainment <djav1985@gmail.com>
2025-07-04 21:11:54 -0400

    Update MU plugin updater to handle direct ZIP

--- mu-plugin/v-sys-plugin-updater-mu.php
+++ mu-plugin/v-sys-plugin-updater-mu.php
@@ -37,64 +37,57 @@ function vontmnt_plugin_updater_run_updates(): void {
 	if ( ! is_main_site() ) {
 		return;
 	}
-	$plugins = get_plugins();
-	foreach ( $plugins as $plugin_path => $plugin ) {
-		$plugin_slug       = dirname( $plugin_path );
-		$installed_version = $plugin['Version'];
-		$api_url           = add_query_arg(
-			array(
-				'domain'  => rawurlencode( wp_parse_url( site_url(), PHP_URL_HOST ) ),
-				'plugin'  => rawurlencode( $plugin_slug ),
-				'version' => rawurlencode( $installed_version ),
-				'key'     => VONTMENT_KEY,
-			),
-			VONTMENT_PLUGINS
-		);
+       $plugins = get_plugins();
+       foreach ( $plugins as $plugin_path => $plugin ) {
+               $plugin_slug       = dirname( $plugin_path );
+               $installed_version = $plugin['Version'];
+               $api_url           = add_query_arg(
+                       array(
+                               'type'    => 'plugin',
+                               'domain'  => rawurlencode( wp_parse_url( site_url(), PHP_URL_HOST ) ),
+                               'slug'    => rawurlencode( $plugin_slug ),
+                               'version' => rawurlencode( $installed_version ),
+                               'key'     => VONTMENT_KEY,
+                       ),
+                       VONTMENT_PLUGINS
+               );
 
-                // Use wp_remote_get instead of cURL.
-                $response      = wp_remote_get( $api_url );
-		$http_code     = wp_remote_retrieve_response_code( $response );
-		$response_body = wp_remote_retrieve_body( $response );
+               // Use wp_remote_get instead of cURL.
+               $response      = wp_remote_get( $api_url );
+               $http_code     = wp_remote_retrieve_response_code( $response );
+               $response_body = wp_remote_retrieve_body( $response );
 
-		if ( 204 === $http_code ) {
-			continue;
-		} elseif ( 401 === $http_code ) {
-			continue;
-		} elseif ( ! empty( $response_body ) ) {
-			$response_data = json_decode( $response_body, true );
+               if ( 200 === $http_code && ! empty( $response_body ) ) {
+                       require_once ABSPATH . 'wp-admin/includes/file.php';
+                       $upload_dir      = wp_upload_dir();
+                       $plugin_zip_file = $upload_dir['path'] . '/' . basename( $plugin_path ) . '.zip';
+                       file_put_contents( $plugin_zip_file, $response_body );
 
-			if ( isset( $response_data['zip_url'] ) ) {
-				$download_url = $response_data['zip_url'];
+                       global $wp_filesystem;
+                       if ( empty( $wp_filesystem ) ) {
+                               require_once ABSPATH . '/wp-admin/includes/file.php';
+                               WP_Filesystem();
+                       }
 
-				require_once ABSPATH . 'wp-admin/includes/file.php';
-				$upload_dir      = wp_upload_dir();
-				$tmp_file        = download_url( $download_url );
-				$plugin_zip_file = $upload_dir['path'] . '/' . basename( $download_url );
+                       require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
+                       $upgrader = new Plugin_Upgrader();
+                       add_filter(
+                               'upgrader_package_options',
+                               function ( $options ) use ( $plugin_zip_file ) {
+                                       $options['package']           = $plugin_zip_file;
+                                       $options['clear_destination'] = true;
+                                       return $options;
+                               }
+                       );
+                       $upgrader->install( $plugin_zip_file );
+                       remove_all_filters( 'upgrader_package_options' );
 
-				// Move the downloaded file to the uploads directory using WP_Filesystem.
-				global $wp_filesystem;
-				if ( empty( $wp_filesystem ) ) {
-					require_once ABSPATH . '/wp-admin/includes/file.php';
-					WP_Filesystem();
-				}
-				$wp_filesystem->move( $tmp_file, $plugin_zip_file, true );
-
-				require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
-				$upgrader = new Plugin_Upgrader();
-				add_filter(
-					'upgrader_package_options',
-					function ( $options ) use ( $plugin_zip_file ) {
-						$options['package']           = $plugin_zip_file;
-						$options['clear_destination'] = true;
-						return $options;
-					}
-				);
-				$upgrader->install( $plugin_zip_file );
-				remove_all_filters( 'upgrader_package_options' );
-
-				// Delete the plugin zip file using wp_delete_file.
-				wp_delete_file( $plugin_zip_file );
-			}
-		}
-	}
+                       // Delete the plugin zip file using wp_delete_file.
+                       wp_delete_file( $plugin_zip_file );
+               } elseif ( 204 === $http_code ) {
+                       continue;
+               } else {
+                       break;
+               }
+       }
 }
