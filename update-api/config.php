9bdb4187cd65bf0a118d31d695e59795566484b2
Vontainment <djav1985@gmail.com>
2025-07-05 07:37:33 -0400

    Fix HOSTS_ACL path

--- update-api/config.php
+++ update-api/config.php
@@ -6,7 +6,7 @@ define('VALID_PASSWORD', 'admin');
 define('SESSION_TIMEOUT_LIMIT', 1800);
 
 define('BASE_DIR', dirname($_SERVER['DOCUMENT_ROOT']));
-define('HOSTS_ACL', BASE_DIR . '/');
+define('HOSTS_ACL', BASE_DIR);
 define('PLUGINS_DIR', BASE_DIR . '/storage/plugins');
 define('THEMES_DIR', BASE_DIR . '/storage/themes');
 define('BLACKLIST_DIR', BASE_DIR . '/storage');
