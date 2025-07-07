56f88cb4a8d1898f01183cf59e865ec558ef7e7d
nikolai@vontainment.com <nikolai@vontainment.com>
2025-07-07 02:45:05 -0400

    modified:   update-api/app/Models/ThemeModel.php

--- update-api/app/Models/ThemeModel.php
+++ update-api/app/Models/ThemeModel.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Project: UpdateAPI
+ * Author:  Vontainment <services@vontainment.com>
+ * License: https://opensource.org/licenses/MIT MIT License
+ * Link:    https://vontainment.com
+ * Version: 3.0.0
+ *
+ * File: ThemeModel.php
+ * Description: WordPress Update API
+ */
+
 namespace App\Models;
 
 class ThemeModel
