776fdde94a2386c64f205b646511d7a8d256b698
Vontainment <djav1985@gmail.com>
2025-07-07 00:45:34 -0400

    Remove loader and add middleware

--- update-api/app/Core/Router.php
+++ update-api/app/Core/Router.php
@@ -1,11 +1,17 @@
 <?php
 namespace App\Core;
 
+use App\Core\AuthMiddleware;
+
 class Router
 {
     public function dispatch(string $uri): void
     {
         $route = strtok($uri, '?');
+
+        if ($route !== '/login') {
+            AuthMiddleware::check();
+        }
         switch ($route) {
             case '/login':
                 \App\Controllers\AuthController::handleRequest();
--- update-api/public/index.php
+++ update-api/public/index.php
@@ -1,5 +1,4 @@
 <?php
-use App\Lib\Loader;
 use App\Core\Router;
 
 $secureFlag = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off');
@@ -13,7 +12,6 @@ session_regenerate_id(true);
 
 require_once '../config.php';
 require_once '../../vendor/autoload.php';
-require_once '../app/Lib/Loader.php';
 
 $router = new Router();
 $router->dispatch($_SERVER['REQUEST_URI']);
