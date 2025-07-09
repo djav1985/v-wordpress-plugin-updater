167c83f069a998ebd5bfd2e4e1216edee55c0b04
Vontainment <djav1985@gmail.com>
2025-07-09 09:46:28 -0400

    Fix message display method call

--- update-api/app/Views/layouts/footer.php
+++ update-api/app/Views/layouts/footer.php
@@ -18,6 +18,6 @@
     <p>&copy; <?php echo date("Y"); ?> Vontainment. All Rights Reserved.</p>
 </footer>
 <script src="/assets/js/footer-scripts.js"></script>
-<?php echo App\Core\ErrorMiddleware::displayAndClearMessages(); ?>
+<?php App\Core\ErrorMiddleware::displayAndClearMessages(); ?>
 </body>
 </html>
