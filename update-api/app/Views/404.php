337bcaebbc4538952a24f1b766962dddb3449243
nikolai@vontainment.com <nikolai@vontainment.com>
2025-07-09 08:23:52 -0400

    modified:   update-api/app/Views/404.php

--- update-api/app/Views/404.php
+++ update-api/app/Views/404.php
@@ -12,10 +12,24 @@
  * Description: WordPress Update API
  */
 
-require_once __DIR__ . '/layouts/header.php';
 ?>
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="noindex, nofollow">
+    <title>API Update Admin Login</title>
+    <script src="/assets/js/header-scripts.js"></script>
+    <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="stylesheet" href="/assets/css/mobile.css">
+    <link rel="stylesheet" href="/assets/css/login.css">
+</head>
+<body>
 <div class="content-box">
     <h2>404 - Page Not Found</h2>
     <p>The page you are looking for does not exist.</p>
 </div>
-<?php require_once __DIR__ . '/layouts/footer.php'; ?>
+</body>
+</html>
