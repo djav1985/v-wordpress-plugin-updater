39b2cd4f5df4fbc3e392554b294d06a5a03b7d85
nikolai@vontainment.com <nikolai@vontainment.com>
2025-08-14 23:06:41 -0400

    modified:   update-api/app/Controllers/ApiController.php 	modified:   update-api/app/Controllers/LoginController.php 	modified:   update-api/app/Controllers/LogsController.php 	modified:   update-api/app/Core/Controller.php 	modified:   update-api/app/Core/DatabaseManager.php 	modified:   update-api/app/Helpers/Encryption.php 	modified:   update-api/app/Helpers/MessageHelper.php 	modified:   update-api/app/Helpers/Validation.php 	modified:   update-api/app/Models/HostsModel.php 	modified:   update-api/app/Models/LogModel.php 	modified:   update-api/app/Models/ThemeModel.php 	modified:   update-api/app/Views/404.php 	modified:   update-api/config.php 	modified:   update-api/public/index.php

--- update-api/app/Controllers/ApiController.php
+++ update-api/app/Controllers/ApiController.php
@@ -6,7 +6,7 @@
  * Author:  Vontainment <services@vontainment.com>
  * License: https://opensource.org/licenses/MIT MIT License
  * Link:    https://vontainment.com
- * Version: 3.0.0
+ * Version: 4.0.0
  *
  * File: ApiController.php
  * Description: WordPress Update API
