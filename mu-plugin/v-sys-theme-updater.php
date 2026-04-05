e71c0dd46c732978469bcfc639bb7043834a92ec
Nikolai X. Shadeauxs <nikolai@vontainment.com>
2026-04-05 19:27:03 -0400

    deleted:    mu-plugin/v-sys-plugin-updater.php 	deleted:    mu-plugin/v-sys-theme-updater.php

--- mu-plugin/v-sys-theme-updater.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Plugin Name: V Sys Theme Updater
- * Description: Provides helper hooks for theme update testing.
- *
- * Project: UpdateAPI
- * Author:  Vontainment <services@vontainment.com>
- */
